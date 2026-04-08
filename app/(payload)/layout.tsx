eee26a778c7f338d823368f5020110522aadf0e0
GeraldMisa27 <geraldmisa828@gmail.com>
2026-04-08 16:32:39 -0400

    fix: serverFunction async en Payload layout

---
+++ app/(payload)/layout.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import configPromise from "@payload-config";
+import { handleServerFunctions, RootLayout } from "@payloadcms/next/layouts";
+import "@payloadcms/next/css";
+import { importMap } from "./importMap";
+
+type Args = {
+  children: React.ReactNode;
+};
+
+const serverFunction = async (args: any) => {
+  "use server";
+  return handleServerFunctions({
+    ...args,
+    config: configPromise,
+    importMap,
+  });
+};
+
+export default async function Layout({ children }: Args) {
+  return (
+    <RootLayout
+      config={configPromise}
+      importMap={importMap}
+      serverFunction={serverFunction}
+    >
+      {children}
+    </RootLayout>
+  );
+}
