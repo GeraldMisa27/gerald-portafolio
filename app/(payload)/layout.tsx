3cafe1b6230943bf69442f44d1160bcba2a1f197
GeraldMisa27 <geraldmisa828@gmail.com>
2026-04-11 02:16:00 -0400

    feat add css payload

--- app/(payload)/layout.tsx
+++ app/(payload)/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import configPromise from "@payload-config";
 import { handleServerFunctions, RootLayout } from "@payloadcms/next/layouts";
 import type { ServerFunctionArgs } from "payload";
-//import "@payloadcms/next/css";
+import "@payloadcms/next/css";
 import { importMap } from "./importMap";
 
 const serverFunction = async (args: ServerFunctionArgs) => {
